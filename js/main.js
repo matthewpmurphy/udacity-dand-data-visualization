1c8139492cbc0eb6248fd87ca718b4a5443630c6
Matthew Murphy <gonzostew@gmail.com>
2018-03-19 09:40:02 -0500

    new index

--- js/main.js
+++ js/main.js
@@ -1,49 +1,76 @@
 (function($) {
-    'use strict'
-    //draw our chart
-    function draw(data) {
-        'use strict';
-        var minOnTimeValue = (function(data, field) {
-        var minimum = 1;
-        data.forEach(function(record) {
-            if (record[field] < minimum) {
-                minimum = record[field];
-            }
-        });
-        return minimum;
-        })(data, 'on_time');
-        var minY = Math.round(minOnTimeValue*10)/10,
-            maxY = 1;
-        var width = 960,
-            height = 640;
-        var svg = dimple.newSvg('#content', 960, 640);
-        var myChart = new dimple.chart(svg, data);
-        // set x axis
-        var x = myChart.addTimeAxis('x', 'year');
-        x.tickFormat = '%Y';
-        x.title = 'Year';
-        // set y axis
-        var y = myChart.addMeasureAxis('y', 'on_time');
-        y.tickFormat = '%';
-        y.overrideMin = minY;
-        y.overrideMax = maxY;
-        y.title = 'Percentage of Arrivals on Time (within 15 minutes)';
-        myChart.addSeries('carrier_name', dimple.plot.line);
-        myChart.addSeries('carrier_name', dimple.plot.scatter);
-        myChart.addLegend(width*0.65, 75, width*0.25, 75, 'right');
-        myChart.draw();
-    }
+  'use strict'
 
-    //Use D3 to load CSV file and use `draw` callback
-    d3.csv("/data/data.csv", function(d) {
-        return {
-            year: d.year,
-            carrier_name: d.carrier_name,
-            on_time: +d.on_time,
-            arrivals: +d.arrivals
-        };
-    }, function(error, data) {
-        // console.log(error);
-        draw(data);
+  var draw = (data) => {
+      var width = "100%",
+          height = 800;
+      var svg = dimple.newSvg('#chart', width, height);
+      var myChart = new dimple.chart(svg, data);
+
+      // set y axis
+      var minY = 0.70,
+          maxY = .90;
+      var y = myChart.addMeasureAxis('y', 'On Time');
+      y.tickFormat = '%';
+      y.overrideMin = minY;
+      y.overrideMax = maxY;
+      y.title = 'Percentage of Arrivals on Time (within 15 minutes)';
+
+      // set x axis
+      var x = myChart.addTimeAxis('x', 'Year');
+      x.tickFormat = '%Y';
+      x.title = 'Year';
+
+      // set series and legend
+      var s = myChart.addSeries('Carrier Name', dimple.plot.scatter);
+      var p = myChart.addSeries('Carrier Name', dimple.plot.line);
+      var legend = myChart.addLegend("10%", "5%", "80%", "20%", "right");
+
+      // draw
+      myChart.draw();
+
+      // handle mouse events on gridlines
+      y.gridlineShapes.selectAll('line')
+      .style('opacity', 0.25)
+      .on('mouseover', function(e) {
+        d3.select(this)
+          .style('opacity', 1);
+      }).on('mouseleave', function(e) {
+        d3.select(this)
+          .style('opacity', 0.25);
+      });
+
+    // handle mouse events on paths
+    d3.selectAll('path')
+      .style('opacity', 0.25)
+      .on('mouseover', function(e) {
+        d3.select(this)
+          .style('stroke-width', '8px')
+          .style('opacity', 1)
+          .attr('z-index', '1');
+    }).on('mouseleave', function(e) {
+        d3.select(this)
+          .style('stroke-width', '2px')
+          .style('opacity', 0.25)
+          .attr('z-index', '0');
     });
+  }
+
+  var createViz = () => {
+    d3.csv("/data/data.csv", function(d) {
+      return {
+        'Year': d.year,
+        'Carrier Name': d.carrier_name,
+        'On Time': +d.on_time,
+        'Arrivals': +d.arrivals
+      };
+    }, function(data) {
+      // set svg
+      draw(data);
+      events();
+    });
+  }
+
+  createViz();
+
 }).apply(this, [jQuery]);
